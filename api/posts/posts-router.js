58b7fef4fb31860a38cb66a8e663e1654c2ce1b7
NeNeTheCreative <shecodes@nenethecreative.com>
2021-09-28 18:05:46 -0400

    GET EP created

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,10 +1,20 @@
 // implement your posts router here
 const express = require('express')
+const Post = require('./posts-model')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.json('foo')
+    Post.find()
+    .then(posts => {
+       res.json(posts)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The post information could not be retrieve",
+            err: err
+        })
+    })
 
 })
 router.get('/:id', (req, res) => {
