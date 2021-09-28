f9da522e99c64a96f961c52fc628e0e034e44031
NeNeTheCreative <shecodes@nenethecreative.com>
2021-09-28 18:32:42 -0400

    2nd GET EP created

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -17,9 +17,26 @@ router.get('/', (req, res) => {
     })
 
 })
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const posts = await Post.findById(id)
+        if (!posts) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            res.status(200).json(posts)
+        }
 
+    } catch(err) {
+        res.status(500).json({
+            message: "The post information could not be retrieve",
+            err:err
+        })
+    }
 })
+
 router.post('/', (req, res) => {
 
 })
